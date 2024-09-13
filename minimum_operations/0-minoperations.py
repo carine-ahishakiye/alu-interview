da5d80e19691e67a02e6fb879e97a496ccbae0a2
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-09-13 20:45:12 +0200

    minimum-operation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+
 def minOperations(n):
     if n <= 1:
         return 0
 
     ops = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             ops += factor
             n //= factor
         factor += 1
-        
+
     return ops
 
+if __name__ == "__main__":
+    print(minOperations(4))  # Test with example values
+    print(minOperations(12))
