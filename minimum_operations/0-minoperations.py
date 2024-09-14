26568e10bb765790a8513707aaa176b191f47315
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-09-14 08:00:43 +0200

    minoperation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+""" Minimum Operations"""
+
+
 def minOperations(n):
+    """ Minimum Operations"""
     if n <= 1:
         return 0
-
-    ops = 0
-    factor = 2
-    
-    while n > 1:
-        while n % factor == 0:
-            ops += factor
-            n //= factor
-        factor += 1
-        
-    return ops
-
+    i = 2
+    result = 0
+    while i <= n:
+        if n % i == 0:
+            result += i
+            n = n / i
+        else:
+            i += 1
+    return result
