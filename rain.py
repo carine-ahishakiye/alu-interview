f7aad9433c6800585cc26ad3400880d5562d6494
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-09-28 20:27:26 +0200

    rain

--- rain.py
+++ rain.py
@@ -1,29 +1,19 @@
+#!/usr/bin/python3
+"""calculates how much rain will be trapped after it rains
+"""
+
+
 def rain(walls):
-    if not walls:
+    """calculates how much rain will be trapped after it rains
+    """
+    if not walls or len(walls) < 3:
         return 0
-    
-    left, right = 0, len(walls) - 1
-    left_max, right_max = 0, 0
-    water_retained = 0
 
-    while left < right:
-        if walls[left] < walls[right]:
-            if walls[left] >= left_max:
-                left_max = walls[left]
-            else:
-                water_retained += left_max - walls[left]
-            left += 1
-        else:
-            if walls[right] >= right_max:
-                right_max = walls[right]
-            else:
-                water_retained += right_max - walls[right]
-            right -= 1
-
-    return water_retained
-
-if __name__ == "__main__":
-    walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls))  # Output: 6
-    walls = [2, 0, 0, 4, 0, 0, 1, 0]
-    print(rain(walls))  # Output: 6
+    rain = 0
+    for i in range(1, len(walls) - 1):
+        left = max(walls[:i])
+        right = max(walls[i + 1:])
+        min_wall = min(left, right)
+        if walls[i] < min_wall:
+            rain += min_wall - walls[i]
+    return rain
